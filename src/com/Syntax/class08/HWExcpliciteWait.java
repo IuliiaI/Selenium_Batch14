9a80956394453ea9c9f34ebac61c90459466ae74
IuliiaI <iuliiaiaremko@gmail.com>
2022-11-17 18:42:48 -0500

    Selenium Class Excplicite Wait, Static and Dynamic tables

---
+++ src/com/Syntax/class08/HWExcpliciteWait.java
@@ -0,0 +1,94 @@
+package com.Syntax.class08;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class HWExcpliciteWait {
+    /* go to https://the-internet.herokuapp.com/dynamic_controls
+       click on checkbox and click on remove
+       verify the text
+       click on enable verify the textbox is enabled
+       enter text and click disable
+       verify the textbox is disable */
+    public static void main(String[] args) {
+        // set the path to the driver to link it with our class    on mac u dont need .exe on windows u need .exe
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        // create a WebDriver instance
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        //  maximize
+        driver.manage().window().maximize();
+        //  go to the website
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //click on checkbox
+        WebElement checkBox = driver.findElement(By.xpath("//div[@id='checkbox']"));
+        checkBox.click();
+        //click on remove
+
+        //WebDriverWait wait=new WebDriverWait(driver,20);
+        //wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Remove']")));
+
+        WebElement removeBtn = driver.findElement(By.xpath("//button[text()='Remove']"));
+        removeBtn.click();
+        //verify the text
+        WebElement text = driver.findElement(By.xpath("//p[@id='message']"));
+        String textMsg = text.getText();
+        if(textMsg.equalsIgnoreCase("It's gone!")){
+            System.out.println("Text is verified");
+        }
+        //click on enable
+        WebElement enableBtn = driver.findElement(By.xpath("//button[text()='Enable']"));
+        enableBtn.click();
+        //verify the textbox is enabled
+        WebElement textBx = driver.findElement(By.xpath("//input[@type='text']"));
+        WebDriverWait wait1=new WebDriverWait(driver,20);
+        wait1.until(ExpectedConditions.elementToBeClickable(textBx));
+
+
+        System.out.println(textBx.isEnabled());
+        //enter text
+        textBx.sendKeys("Test");
+        //click disable
+        WebElement disableBtn = driver.findElement(By.xpath("//button[text()='Disable']"));
+        disableBtn.click();
+        //verify the textbox is disable
+
+        WebElement disabledText = driver.findElement(By.xpath("//p[@id='message']"));
+
+        WebDriverWait wait2=new WebDriverWait(driver,20);
+        wait2.until(ExpectedConditions.visibilityOf(disabledText));
+
+        System.out.println(textBx.isEnabled());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/Syntax/class09/HWDynamicTable.java
@@ -0,0 +1,61 @@
+package com.Syntax.class09;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class HWDynamicTable {
+    /*        http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login
+      got to the url
+      click on PIM
+      click on Employee List
+      from the table choose one id (it must be from the first page)
+      and check the checkBox for that id   (your logic should be dynamic  , should work for any ID number)
+
+      the user name is
+      admin
+      the password is
+      Hum@nhrm123            */
+    public static void main(String[] args) {
+        //        set the path to the driver to link it with our class    on mac u dont need .exe on windows u need .exe
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        // create a WebDriver instance
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//        maximize
+        driver.manage().window().maximize();
+        //  go to the website
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+        //enter credentials and login to the web App
+        WebElement usernameTextBx = driver.findElement(By.xpath("//input[@id='txtUsername']"));
+        usernameTextBx.sendKeys("admin");
+        WebElement passworTextBx = driver.findElement(By.xpath("//input[@id='txtPassword']"));
+        passworTextBx.sendKeys("Hum@nhrm123");
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@value='LOGIN']"));
+        loginBtn.click();
+        //click on PIM
+        WebElement pimTextBx = driver.findElement(By.xpath("//b[text()='PIM']"));
+        pimTextBx.click();
+        //click on Employee List
+        WebElement empListTextBx = driver.findElement(By.xpath("//a[text()='Employee List']"));
+        empListTextBx.click();
+        //from the table choose one id (it must be from the first page) - 45137A
+        //      and check the checkBox for that id   (your logic should be dynamic  , should work for any ID number)
+        List<WebElement> columnSecond = driver.findElements(By.xpath("//table[@id='resultTable']/tbody/tr/td[2]"));
+        for(int i=0;i< columnSecond.size();i++){
+            String columnText = columnSecond.get(i).getText();
+
+            if(columnText.equalsIgnoreCase("45137A")){
+                System.out.println("the index of the row which contains 45137A is: "+(i+1));
+                // //table[@id='resultTable']/tbody/tr[5]/td[1]
+                WebElement checkBox = driver.findElement(By.xpath("//table[@id='resultTable']/tbody/tr[" + (i + 1) + "]/td[1]"));
+                checkBox.click();
+            }
+        }
+
+    }
+}
